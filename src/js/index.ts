93860c94c3fcca9329e888b15a00b180889a3c07
Milena Ognyanova <mile0416@edu.easj.dk>
2018-10-10 09:24:18 +0200

    mimi

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,3 +1,4 @@
+/*
 interface Person {
     firstName: string;
     lastName: string;
@@ -10,3 +11,25 @@ let user: Person = { firstName: "John", lastName: "Doe" };
 
 let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
 element.innerHTML = greeter(user);
+*/
+
+let weightInput: HTMLInputElement = <HTMLInputElement>document.getElementById("weightInput");
+let convertToGramsButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("convertToGramsButton");
+let convertToOuncesButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("convertToOuncesButton");
+let output : HTMLDivElement = <HTMLDivElement>document.getElementById("outputDiv");
+
+
+convertToGramsButton.addEventListener("click", convertToGrams);
+convertToOuncesButton.addEventListener("click", convertToOunces);
+
+function convertToGrams(): void{
+    let value1: string = weightInput.value;
+    let result: number = parseInt(value1) *  28.34952;
+    output.innerHTML = result.toString();
+}
+
+function convertToOunces(): void{
+    let value1: string = weightInput.value;
+    let result: number = parseInt(value1) / 28.34952;
+    output.innerHTML = result.toString();
+}
